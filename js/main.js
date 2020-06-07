3dd26c878372aef7e74e8b0b64311e0c13296754
Маша <makrosensei@gmail.com>
2020-06-07 22:17:03 +0300

    Зад.8

--- js/main.js
+++ js/main.js
@@ -4,22 +4,26 @@ var NUMBER_OF_ADS = 8;
 var PLACE_TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var ROOMS_NUMBERS = [1, 2, 3];
 var GUESTS_NUMBERS = [1, 2, 3];
-var PLACE_CHECKIN = ['12:00', '13:00', '14:00'];
-var PLACE_CHECKOUT = ['12:00', '13:00', '14:00'];
+var PLACES_CHECKIN = ['12:00', '13:00', '14:00'];
+var PLACES_CHECKOUT = ['12:00', '13:00', '14:00'];
 var PLACE_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PLACE_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var LOCATION_X_MIN = 0;
+var LOCATION_X_MAX = 1200;
+var LOCATION_Y_MIN = 130;
+var LOCATION_Y_MAX = 630;
 
 var switchToActiveState = function () {
   var map = document.querySelector('.map');
   map.classList.remove('map--faded');
 };
 
-var getRandomElement = function (elements) {
-  return elements[Math.floor(Math.random() * elements.length)];
-};
-
 var getRandomIndex = function (elements) {
   return Math.floor(Math.random() * elements.length);
+};
+
+var getRandomElement = function (elements) {
+  return elements[getRandomIndex(elements)];
 };
 
 var getRandomNumberInInterval = function (min, max) {
@@ -27,36 +31,46 @@ var getRandomNumberInInterval = function (min, max) {
 };
 
 var shuffleArray = function (elements) {
-  elements.sort();
-  elements.slice(getRandomIndex(elements));
+  for (var i = elements.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var l = elements[i];
+    elements[i] = elements[j];
+    elements[j] = l;
+  }
+  // Это ничего что я использую такие странные переменные в этой функции?
   return elements;
+};
+
+var pruningArray = function (elements) {
+  shuffleArray(elements);
+  elements.slice(getRandomIndex(elements));
 };
 
 var generateSimilarAd = function (quantity) {
   var similarAds = [];
   for (var i = 1; i <= quantity; i++) {
-    var COORDINATE_X = getRandomNumberInInterval(0, 1200);
-    var COORDINATE_Y = getRandomNumberInInterval(130, 630);
+    var coordinateX = getRandomNumberInInterval(LOCATION_X_MIN, LOCATION_X_MAX);
+    var coordinateY = getRandomNumberInInterval(LOCATION_Y_MIN, LOCATION_Y_MAX);
     var item = {
       author: {
         avatar: 'img/avatars/user0' + i + '.png'
       },
       offer: {
         title: '',
-        address: COORDINATE_X + ', ' + COORDINATE_Y,
+        address: coordinateX + ', ' + coordinateY,
         price: getRandomNumberInInterval(1000, 100000),
         type: getRandomElement(PLACE_TYPES),
         rooms: getRandomElement(ROOMS_NUMBERS),
         guests: getRandomElement(GUESTS_NUMBERS),
-        checkin: getRandomElement(PLACE_CHECKIN),
-        checkout: getRandomElement(PLACE_CHECKOUT),
-        features: shuffleArray(PLACE_FEATURES),
+        checkin: getRandomElement(PLACES_CHECKIN),
+        checkout: getRandomElement(PLACES_CHECKOUT),
+        features: pruningArray(PLACE_FEATURES),
         description: '',
-        photos: shuffleArray(PLACE_PHOTOS)
+        photos: pruningArray(PLACE_PHOTOS)
       },
       location: {
-        x: COORDINATE_X,
-        y: COORDINATE_Y
+        x: coordinateX,
+        y: coordinateY
       }
     };
     similarAds.push(item);
@@ -67,10 +81,9 @@ var generateSimilarAd = function (quantity) {
 var getPin = function (element) {
   var pin = document.querySelector('#pin').content.querySelector('.map__pin');
   var ad = pin.cloneNode(true);
-  // ad.style = 'left: 100px; top: 100px;';
-  // Это работает ↑
-  // А это он почему-то не понимает ↓
-  ad.style = 'left: ' + element.location.x - 25 + ' px; top: ' + element.location.y - 70 + ' px;';
+
+  // Наверное я просто не так поняла, но оно не работает.. ↓
+  ad.style = 'left: ' + (element.location.x - 25) + ' px; top: ' + (element.location.y - 70) + ' px;';
   ad.querySelector('img').src = element.author.avatar;
   ad.querySelector('img').alt = element.offer.title;
 
